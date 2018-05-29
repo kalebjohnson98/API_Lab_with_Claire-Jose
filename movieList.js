916e7d3d421943181b5bf9de9879bcbc5d35f1cd
kalebjohnson98 <kaleb.j.business@gmail.com>
2018-05-29 11:03:51 -0400

    initial commit

---
+++ movieList.js
@@ -0,0 +1,6 @@
+"use strict";
+{
+    angular
+        .module("movie")
+        .angular("movieList", movieList);
+}
---
+++ watchlistPage.js
@@ -0,0 +1,6 @@
+"use strict";
+{
+    angular
+        .module("movie")
+        .angular("watchListPage", watchListPage);
+}
