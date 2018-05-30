52b0f3ba240c8fae54f4d1a951b11b6eaa9ad318
clairose <clairose@umich.edu>
2018-05-30 14:53:39 -0400

    Merge

--- apiService.js
+++ apiService.js
@@ -2,7 +2,7 @@
 {
   function movieapi($http) {
     let finalData = {};
-    const searchWeather = (genre) => {
+    const setMovie = () => {
       return $http({
         method: 'GET',
         url: 'https://api.themoviedb.org/3/search/movie?api_key=a53a6bc42972a9ac76757aa3b5089827&query=' + genre
@@ -14,11 +14,11 @@
       });
     };
     
-    const returnResults = () => { return finalData; };
+    const getMovie = () => { return finalData; };
     
     return {
       returnResults,
-      searchWeather
+      searchMovies
     };
     
   }
--- searchCriteria.js
+++ searchCriteria.js
@@ -3,15 +3,21 @@
     let searchCriteria = {
         templateUrl: "searchCriteria.html",
         
-        controller: function ($scope) {
+        controller: function ($scope, movieapi) {
+                const vm = this;
                 $scope.movie = [
                     {type: "Genre", options: ["Action", "Comedy", "Romance", "Thriller"]},
                     {type: "Length", options: ["1-1:30 Hours", "1:30-2 Hours"]},
                     {type: "Rating"}
                 ];
+                vm.results = function (){
+                    vm.result = movieapi.returnResults();
+                    console.log(result);
+                }
+                
         }
     }
-    //   movieapi.$inject = ["$http"];
+        searchCriteria.$inject = ["movieapi"];
     angular
         .module("movie")
         .component("searchCriteria", searchCriteria);
